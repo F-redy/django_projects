4ff4854566d9a67f3bd8e31b4e0f3bb82e77acd4
Stanislav <nstanislass@gmail.com>
2023-04-19 03:14:13 +0300

    join users

--- password_generator_project/password_generator_project/urls.py
+++ password_generator_project/password_generator_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('generator/', include('generator.urls', namespace='generator')),
+    path('', include('generator.urls', namespace='generator')),
+    path('users/', include('users.urls', namespace='users')),
 ]
