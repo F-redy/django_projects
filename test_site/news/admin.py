f30c2c3d39e28fae96cbb8d04fc5822346aca22f
Stanislav <nstanislass@gmail.com>
2023-05-01 17:19:49 +0300

    add NewsAdmin, Category

--- test_site/news/admin.py
+++ test_site/news/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from news.models import News, Category
 
-# Register your models here.
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'cat', 'created_at', 'updated_at', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+    list_editable = ('is_published', 'cat')
+    list_filter = ('cat',)
+
+
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    list_display = ('id', 'title', 'slug')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
