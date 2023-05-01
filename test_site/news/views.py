707907f9847a6f01c7b8d681e80bbb8627a699b1
Stanislav <nstanislass@gmail.com>
2023-05-01 17:15:44 +0300

    fix func index, add func get_category

--- test_site/news/views.py
+++ test_site/news/views.py
@@ -1,13 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import News
+from .models import News, Category
 
 
 def index(request):
-    news = News.objects.order_by('-created_at')
+    news = News.objects.all()
     title = 'Список новостей'
     context = {
         'news_list': news,
         'title': title,
+        'cat_selected': 0,
+    }
+    return render(request, 'news/index.html', context=context)
+
+
+def get_category(request, cat_slug):
+    category = Category.objects.get(slug=cat_slug)
+    news = News.objects.filter(cat_id=category.pk)
+    context = {
+        'news_list': news,
+        'title': category,
+        'cat_selected': category.pk,
     }
     return render(request, 'news/index.html', context=context)
