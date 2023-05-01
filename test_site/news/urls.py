669ee5d6eca021032d6e1116165cb87c302a6945
Stanislav <nstanislass@gmail.com>
2023-05-01 17:16:59 +0300

    add path category

--- test_site/news/urls.py
+++ test_site/news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from news.views import index
+from news.views import index, get_category
 
 app_name = 'news'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('category/<slug:cat_slug>/', get_category, name='category'),
 ]
