cddc53ad0a9c70806838db2d10738f19ca00cb72
Stanislav <nstanislass@gmail.com>
2023-05-01 18:41:03 +0300

    fix get_categories changed simple_tag/inclusion_tag changed name func by show_categories add cat_selected

--- test_site/news/templatetags/news_tags.py
+++ test_site/news/templatetags/news_tags.py
@@ -5,6 +5,6 @@ from news.models import Category
 register = template.Library()
 
 
-@register.simple_tag()
-def get_categories():
-    return Category.objects.all()
+@register.inclusion_tag('news/tags/list_categories.html')
+def show_categories(cat_selected=0):
+    return {'categories': Category.objects.all(), 'cat_selected': cat_selected}
