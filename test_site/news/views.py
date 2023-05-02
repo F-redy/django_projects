a900ffc1a4161ce3811c7ffdf1256ff4dabde204
Stanislav <nstanislass@gmail.com>
2023-05-02 15:38:51 +0300

    fix func get_category; add func show_news

--- test_site/news/views.py
+++ test_site/news/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import News, Category
 
 
@@ -15,11 +14,21 @@ def index(request):
 
 
 def get_category(request, cat_slug):
-    category = Category.objects.get(slug=cat_slug)
+    category = get_object_or_404(Category, slug=cat_slug)
     news = News.objects.filter(cat_id=category.pk)
     context = {
         'news_list': news,
-        'title': category,
+        'title': category.title,
         'cat_selected': category.pk,
     }
     return render(request, 'news/index.html', context=context)
+
+
+def show_news(request, news_slug):
+    news = get_object_or_404(News, slug=news_slug)
+    context = {
+        'news': news,
+        'title': news.title,
+        'cat_selected': news.cat_id
+    }
+    return render(request, 'news/news.html', context=context)
