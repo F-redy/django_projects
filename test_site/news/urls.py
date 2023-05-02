0c76b79a4c5fa054606f0ae4741cc235fe7c4e26
Stanislav <nstanislass@gmail.com>
2023-05-03 02:43:59 +0300

    add path addnews

--- test_site/news/urls.py
+++ test_site/news/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from news.views import index, get_category, show_news
+from news.views import index, get_category, show_news, add_news
 
 app_name = 'news'
 
 urlpatterns = [
     path('', index, name='index'),
     path('category/<slug:cat_slug>/', get_category, name='category'),
-    path('news/<slug:news_slug>/', show_news, name='show_news')
+    path('news/<slug:news_slug>/', show_news, name='show_news'),
+    path('addnews/', add_news, name='add_news'),
+
 ]
