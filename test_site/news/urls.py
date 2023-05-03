36ef393c94d03d8537f2c9eabe2e881194a89cf6
Stanislav <nstanislass@gmail.com>
2023-05-03 18:18:27 +0300

    fix 7 line change HomeView change name path

--- test_site/news/urls.py
+++ test_site/news/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from news.views import index, get_category, show_news, add_news
+from news.views import index, get_category, show_news, add_news, HomeView
 
 app_name = 'news'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', HomeView.as_view(), name='home'),
     path('category/<slug:cat_slug>/', get_category, name='category'),
     path('news/<slug:news_slug>/', show_news, name='show_news'),
     path('addnews/', add_news, name='add_news'),
