80c88f056ba9bd4338d6c17137db914717c32c42
Stanislav <nstanislass@gmail.com>
2023-05-03 14:11:10 +0300

    fix used ModelForm

--- test_site/news/forms.py
+++ test_site/news/forms.py
@@ -1,22 +1,32 @@
 from django import forms
-from .models import Category
+from .models import Category, News
 
 
-class AddNewsForm(forms.Form):
-    title = forms.CharField(
-        label='Название',
-        max_length=250,
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Название новости'}))
+class AddNewsForm(forms.ModelForm):
+    # title = forms.CharField(
+    #     label='Название',
+    #     max_length=250,
+    #     widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Название новости'}))
+    #
+    # content = forms.CharField(
+    #     label='Текст',
+    #     required=False,
+    #     widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Описание новости...'}))
+    #
+    # is_published = forms.BooleanField(label='Опубликовано', initial=True)
+    #
+    # cat = forms.ModelChoiceField(
+    #     label='Категория',
+    #     queryset=Category.objects.all(),
+    #     empty_label='не выбрана',
+    #     widget=forms.Select(attrs={'class': 'form-control'}))
 
-    content = forms.CharField(
-        label='Текст',
-        required=False,
-        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Описание новости...'}))
+    class Meta:
+        model = News
+        fields = ('title', 'content', 'is_published', 'cat')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Название новости'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Описание новости...'}),
+            'cat': forms.Select(attrs={'class': 'form-control'}),
 
-    is_published = forms.BooleanField(label='Опубликовано', initial=True)
-
-    cat = forms.ModelChoiceField(
-        label='Категория',
-        queryset=Category.objects.all(),
-        empty_label='не выбрана',
-        widget=forms.Select(attrs={'class': 'form-control'}))
+        }
