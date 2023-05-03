c23ad743786f0a6cb336884d3dd2605b97ad20bd
Stanislav <nstanislass@gmail.com>
2023-05-03 17:20:24 +0300

    add method clean_title

--- test_site/news/forms.py
+++ test_site/news/forms.py
@@ -1,26 +1,12 @@
+import re
+
 from django import forms
-from .models import Category, News
+from django.core.exceptions import ValidationError
+
+from .models import News
 
 
 class AddNewsForm(forms.ModelForm):
-    # title = forms.CharField(
-    #     label='Название',
-    #     max_length=250,
-    #     widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Название новости'}))
-    #
-    # content = forms.CharField(
-    #     label='Текст',
-    #     required=False,
-    #     widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Описание новости...'}))
-    #
-    # is_published = forms.BooleanField(label='Опубликовано', initial=True)
-    #
-    # cat = forms.ModelChoiceField(
-    #     label='Категория',
-    #     queryset=Category.objects.all(),
-    #     empty_label='не выбрана',
-    #     widget=forms.Select(attrs={'class': 'form-control'}))
-
     class Meta:
         model = News
         fields = ('title', 'content', 'is_published', 'cat')
@@ -30,3 +16,9 @@ class AddNewsForm(forms.ModelForm):
             'cat': forms.Select(attrs={'class': 'form-control'}),
 
         }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название новости не должно начинаться с цифры')
+        return title
