706b6dcc1bab3408019655a9c571e6ca7e78144e
Stanislav <nstanislass@gmail.com>
2023-05-05 15:50:00 +0300

    fix path news

--- test_site/news/urls.py
+++ test_site/news/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from news.views import get_category, show_news, add_news, HomeView, NewsByCategory
+from news.views import add_news, HomeView, NewsByCategory, ViewNews
 
 app_name = 'news'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('category/<slug:cat_slug>/', NewsByCategory.as_view(), name='category'),
-    path('news/<slug:news_slug>/', show_news, name='show_news'),
+    path('news/<slug:news_slug>/', ViewNews.as_view(), name='show_news'),
     path('addnews/', add_news, name='add_news'),
 
 ]
