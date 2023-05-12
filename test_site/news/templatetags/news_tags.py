ee80d2dd54fc2405bab49fdb0039d2b8b104e85a
Stanislav <nstanislass@gmail.com>
2023-05-12 20:45:14 +0300

    add annotate count news

--- test_site/news/templatetags/news_tags.py
+++ test_site/news/templatetags/news_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from news.models import Category
 
@@ -7,4 +8,5 @@ register = template.Library()
 
 @register.inclusion_tag('news/tags/list_categories.html')
 def show_categories(cat_selected=0):
-    return {'categories': Category.objects.all(), 'cat_selected': cat_selected}
+    categories = Category.objects.annotate(Count('news')).filter(news__count__gt=0)
+    return {'categories': categories, 'cat_selected': cat_selected}
