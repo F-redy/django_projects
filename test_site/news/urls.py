437b9cd399110d46d3a437daf71aef6a0779d98e
Stanislav <nstanislass@gmail.com>
2023-05-12 20:43:33 +0300

    add path pagination

--- test_site/news/urls.py
+++ test_site/news/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from news.views import add_news, HomeView, NewsByCategory, ViewNews, CreateNews
+from news.views import HomeView, NewsByCategory, ViewNews, CreateNews
 
 app_name = 'news'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('page/<int:page>/', HomeView.as_view(), name='paginator'),
     path('category/<slug:cat_slug>/', NewsByCategory.as_view(), name='category'),
     path('news/<slug:news_slug>/', ViewNews.as_view(), name='show_news'),
     path('addnews/', CreateNews.as_view(), name='add_news'),
