4e3826809a2677f1057e584b24aa2343955c7cc7
Stanislav <nstanislass@gmail.com>
2023-05-15 13:07:47 +0300

    fix add filter news__is_published

--- test_site/news/templatetags/news_tags.py
+++ test_site/news/templatetags/news_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from django.db.models import Count
+from django.db.models import Count, F
 
 from news.models import Category
 
@@ -8,5 +8,5 @@ register = template.Library()
 
 @register.inclusion_tag('news/tags/list_categories.html')
 def show_categories(cat_selected=0):
-    categories = Category.objects.annotate(Count('news')).filter(news__count__gt=0)
+    categories = Category.objects.filter(news__is_published=True).annotate(Count('news')).filter(news__count__gt=0)
     return {'categories': categories, 'cat_selected': cat_selected}
